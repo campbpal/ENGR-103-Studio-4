5aec75f4cf717665103dcafc0822d06f931478ea
palmerc120 <palmerc120@gmail.com>
2022-04-21 09:55:16 -0700

    Working

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+def main():
+    import pandas as pd
+    file_path = 'C:/Users/palme/Desktop/Spring 2022/energy_use_terrawatthours.csv'
+    energy_use = pd.read_csv(file_path)
+    print(energy_use)
+    year = energy_use.loc[:, 'year']
+    power_source = energy_use.loc[:, "variable"]
+    generated = energy_use.loc[:, "generation_twh"]
+    for x in range(len(year)):
+        if year[x] == 2000:
+            print("Energy from", power_source[x], "accounts for", generated[x], "twh of world power generation in the "
+                                                                                "year", year[x])
 
-# Press Ctrl+R to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    print("\n\n\n")
+    for x in range(len(year)):
+        if (year[x] >= 200) and (year[x] <= 2021) and (power_source[x] == "Clean"):
+            print("Energy from", power_source[x], "accounts for", generated[x], "twh of world power generation in the "
+                                                                                "year", year[x])
+
+    print("\n\n\n")
+    for x in range(len(year)):
+        if (year[x] >= 200) and (year[x] <= 2021) and (power_source[x] == "Fossil"):
+            print("Energy from", power_source[x], "accounts for", generated[x], "twh of world power generation in the "
+                                                                                "year", year[x])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
